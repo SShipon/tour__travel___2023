ee64e112df5fe9551418b81ccd28a32151a2d24b
Nozibul <nozibulislamspi@gmail,com>
2023-02-03 17:16:56 +0600

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <button className="btn btn-active btn-primary">Button</button>
+      <button className="btn btn-active btn-secondary">Button</button>
+       <button className="btn btn-active btn-accent">Button</button>
+    </>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    './pages/**/*.{html,jsx,js}',
+    './components/**/*.{html,jsx,js}',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
